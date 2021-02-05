fc700282bda3dcb5a140c00c49cb5f97112cddae
stone1234765 <51057074+stone1234765@users.noreply.github.com>
2021-02-05 18:42:36 +0200

    add good new style questions

--- SunnyFlamingo/Services/Searchers/GoodsSearchers/GoodsQuestionsService.cs
+++ SunnyFlamingo/Services/Searchers/GoodsSearchers/GoodsQuestionsService.cs
@@ -23,6 +23,43 @@ namespace SunnyFlamingo.Services.Searchers
         }
 
 
+        public async Task<List<QuestionsBase<string>>> GetGoodsQuestions(
+            IQueryable<Good> goods,
+            string[] producers,
+            string[] countries,
+            string[] materials,
+            string[] colors,
+            decimal? priceFrom,
+            decimal? priceTo)
+        {
+            var result = await GetQuestionList(goods, producers, countries, materials, colors, priceFrom, priceTo);
+            return new List<QuestionsBase<string>>()
+            {
+                _questionsGrouper.GroupProducers(result),
+                _questionsGrouper.GroupCountries(result),
+                _questionsGrouper.GroupMaterials(result),
+                _questionsGrouper.GroupColors(result),
+                _questionsGrouper.GroupPrices(result, priceFrom, priceTo),
+            };
+        }
+        private async Task<List<QuestionBase<string>>> GetQuestionList(
+            IQueryable<Good> goods,
+            string[] producers,
+            string[] countries,
+            string[] materials,
+            string[] colors,
+            decimal? priceFrom,
+            decimal? priceTo)
+        {
+            var producerQuestions = GetProducerQuestion(GetProducerGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), producers);
+            var countryQuestions = GetCountryQuestion(GetCountryGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), countries);
+            var materialQuestions = GetMaterialQuestion(GetMaterialGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), materials);
+            var colorQuestions = GetColorQuestion(GetColorGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), colors);
+            var priceQuestions = GetPriceQuestion(GetPriceGoods(goods, producers, countries, materials, colors, priceFrom, priceTo));
+
+            var group = producerQuestions.Union(countryQuestions).Union(materialQuestions).Union(colorQuestions).Union(priceQuestions);
+            return await group.ToListAsync();
+        }
         public IQueryable<T> GetProducerGoods<T>(
             IQueryable<T> goods,
             string[] producers,
@@ -132,45 +169,8 @@ namespace SunnyFlamingo.Services.Searchers
 
 
 
-        public async Task<List<QuestionBase<string>>> GetQuestionList(
-            IQueryable<Good> goods,
-            string[] producers,
-            string[] countries,
-            string[] materials,
-            string[] colors,
-            decimal? priceFrom,
-            decimal? priceTo)
-        {
-            var producerQuestions = GetProducerQuestion2(GetProducerGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), producers);
-            var countryQuestions = GetCountryQuestion2(GetCountryGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), countries);
-            var materialQuestions = GetMaterialQuestion2(GetMaterialGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), materials);
-            var colorQuestions = GetColorQuestion2(GetColorGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), colors);
-            var priceQuestions = GetPriceQuestion2(GetPriceGoods(goods, producers, countries, materials, colors, priceFrom, priceTo));
 
-            var group = producerQuestions.Union(countryQuestions).Union(materialQuestions).Union(colorQuestions).Union(priceQuestions);
-            return await group.ToListAsync();
-        }
-
-        public async Task<List<QuestionsBase<string>>> GetGoodsQuestions2(
-            IQueryable<Good> goods,
-            string[] producers,
-            string[] countries,
-            string[] materials,
-            string[] colors,
-            decimal? priceFrom,
-            decimal? priceTo)
-        {
-            var result = await GetQuestionList(goods, producers, countries, materials, colors, priceFrom, priceTo);
-            return new List<QuestionsBase<string>>()
-            {
-                _questionsGrouper.GroupProducers(result),
-                _questionsGrouper.GroupCountries(result),
-                _questionsGrouper.GroupMaterials(result),
-                _questionsGrouper.GroupColors(result),
-                _questionsGrouper.GroupPrices(result, priceFrom, priceTo),
-            };
-        }
-        private IQueryable<QuestionBase<string>> GetProducerQuestion2(IQueryable<Good> goods, string[] producers)
+        public IQueryable<QuestionBase<string>> GetProducerQuestion(IQueryable<Good> goods, string[] producers)
         {
             return goods
                         .Select(g => g.Producer.Name)
@@ -188,7 +188,7 @@ namespace SunnyFlamingo.Services.Searchers
                             Type = InputType.Checkbox
                         });
         }
-        private IQueryable<QuestionBase<string>> GetCountryQuestion2(IQueryable<Good> goods, string[] countries)
+        public IQueryable<QuestionBase<string>> GetCountryQuestion(IQueryable<Good> goods, string[] countries)
         {
             return goods
                         .Select(g => g.Manufacturer.Country.Value)
@@ -206,7 +206,7 @@ namespace SunnyFlamingo.Services.Searchers
                             Type = InputType.Checkbox,
                         });
         }
-        private IQueryable<QuestionBase<string>> GetMaterialQuestion2(IQueryable<Good> goods, string[] materials)
+        public IQueryable<QuestionBase<string>> GetMaterialQuestion(IQueryable<Good> goods, string[] materials)
         {
             return goods
                         .Select(g => g.Material.Value)
@@ -224,7 +224,7 @@ namespace SunnyFlamingo.Services.Searchers
                             Type = InputType.Checkbox,
                         });
         }
-        private IQueryable<QuestionBase<string>> GetColorQuestion2(IQueryable<Good> goods, string[] colors)
+        public IQueryable<QuestionBase<string>> GetColorQuestion(IQueryable<Good> goods, string[] colors)
         {
             return goods
                         .Select(g => g.Color.Value)
@@ -242,7 +242,7 @@ namespace SunnyFlamingo.Services.Searchers
                             Type = InputType.Checkbox,
                         });
         }
-        public IQueryable<QuestionBase<string>> GetPriceQuestion2(IQueryable<Good> goods)
+        public IQueryable<QuestionBase<string>> GetPriceQuestion(IQueryable<Good> goods)
         {
             var max = goods
                 .Select(g => g.Price)
@@ -278,169 +278,5 @@ namespace SunnyFlamingo.Services.Searchers
             return result;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public async Task<QuestionsBase<string>> GetPriceQuestion(IQueryable<Good> goods, decimal? selectedFrom, decimal? selectedTo)
-        {
-            var min = await goods.Select(good => good.Price).MinAsync();
-            var max = await goods.Select(good => good.Price).MaxAsync();
-            //var minDecimal = decimal.MinValue;
-            //var maxDecimal = decimal.MaxValue;
-            //var maxAndMin = goods
-            //            .Select(g => g.Price)
-            //            .Aggregate(new
-            //            {
-            //                Min = maxDecimal,
-            //                Max = minDecimal
-            //            }, (accumulator, o) => new
-            //            {
-            //                Min = o < accumulator.Min ? o : accumulator.Min,
-            //                Max = o > accumulator.Max ? o : accumulator.Max
-            //            });
-            return new QuestionsBase<string>()
-            {
-                Key = "price",
-                Value = "Select price",
-                Order = 1,
-                QuestionBaseList = new List<QuestionBase<string>>()
-                {
-                    new QuestionBase<string>()
-                        {
-                            ControlType = ControlType.RangeSlider,
-                            From = min,
-                            To = max,
-                            SelectedFrom = selectedFrom == null ? min : selectedFrom,
-                            SelectedTo = selectedTo == null ? max : selectedTo,
-                            Required = false,
-                            Type = InputType.Checkbox,
-                        }
-                }
-            };
-        }
-        public async Task<QuestionsBase<string>> GetProducerQuestion(IQueryable<Good> goods, string[] producers)
-        {
-            return new QuestionsBase<string>()
-            {
-                Key = "producer",
-                Value = "Select producer",
-                Order = 1,
-                QuestionBaseList = await goods
-                        .Select(g => g.Producer.Name)
-                        .GroupBy(p => p)
-                        .Select(p => new { Count = p.Count(), Value = p.Key })
-                        .Select(producer => new QuestionBase<string>()
-                        {
-                            AfterBox = $"({producer.Count})",
-                            Checked = producers != null && producers.Contains(producer.Value),
-                            ControlType = ControlType.Checkbox,
-                            Key = producer.Value,
-                            Label = producer.Value,
-                            Required = false,
-                            Type = InputType.Checkbox,
-                        }).ToListAsync()
-            };
-        }
-        public async Task<QuestionsBase<string>> GetCountryQuestion(IQueryable<Good> goods, string[] countries)
-        {
-            return new QuestionsBase<string>()
-            {
-                Key = "country",
-                Value = "Select country",
-                Order = 2,
-                QuestionBaseList = await goods
-                        .Select(g => g.Manufacturer.Country.Value)
-                        .GroupBy(c => c)
-                        .Select(c => new { Count = c.Count(), Value = c.Key })
-                        .Select(country => new QuestionBase<string>()
-                        {
-                            AfterBox = $"({country.Count})",
-                            Checked = countries != null && countries.Contains(country.Value),
-                            ControlType = ControlType.Checkbox,
-                            Key = country.Value,
-                            Label = country.Value,
-                            Required = false,
-                            Type = InputType.Checkbox,
-                        }).ToListAsync()
-            };
-        }
-        public async Task<QuestionsBase<string>> GetMaterialQuestion(IQueryable<Good> goods, string[] materials)
-        {
-            return new QuestionsBase<string>()
-            {
-                Key = "material",
-                Value = "Select material",
-                Order = 3,
-                QuestionBaseList = await goods
-                        .Select(g => g.MaterialValue)
-                        .GroupBy(m => m)
-                        .Select(m => new { Count = m.Count(), Value = m.Key })
-                        .Select(material => new QuestionBase<string>()
-                        {
-                            AfterBox = $"({material.Count})",
-                            Checked = materials != null && materials.Contains(material.Value),
-                            ControlType = ControlType.Checkbox,
-                            Key = material.Value,
-                            Label = material.Value,
-                            Required = false,
-                            Type = InputType.Checkbox,
-                        }).ToListAsync()
-            };
-        }
-        public async Task<QuestionsBase<string>> GetColorQuestion(IQueryable<Good> goods, string[] colors)
-        {
-            return new QuestionsBase<string>()
-            {
-                Key = "color",
-                Value = "Select color",
-                Order = 4,
-                QuestionBaseList = await goods
-                        .Select(g => g.ColorValue)
-                        .GroupBy(c => c)
-                        .Select(c => new { Count = c.Count(), Value = c.Key })
-                        .Select(color => new QuestionBase<string>()
-                        {
-                            AfterBox = $"({color.Count})",
-                            Checked = colors != null && colors.Contains(color.Value),
-                            ControlType = ControlType.Checkbox,
-                            Key = color.Value,
-                            Label = color.Value,
-                            Required = false,
-                            Type = InputType.Checkbox,
-                        }).ToListAsync()
-            };
-        }
-        public async Task<List<QuestionsBase<string>>> GetGoodsQuestions(
-            IQueryable<Good> goods,
-            string[] producers,
-            string[] countries,
-            string[] materials,
-            string[] colors,
-            decimal? priceFrom,
-            decimal? priceTo)
-        {
-            return new List<QuestionsBase<string>>()
-                {
-                    await GetProducerQuestion(GetProducerGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), producers),
-                    await GetCountryQuestion(GetCountryGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), countries),
-                    await GetMaterialQuestion(GetMaterialGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), materials),
-                    await GetColorQuestion(GetColorGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), colors),
-                    await GetPriceQuestion(GetPriceGoods(goods, producers, countries, materials, colors, priceFrom, priceTo), priceFrom, priceTo)
-                };
-        }
     }
 }
