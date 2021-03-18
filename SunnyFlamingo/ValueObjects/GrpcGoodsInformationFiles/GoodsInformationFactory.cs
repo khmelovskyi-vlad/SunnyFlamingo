dc0e08c6209e3c9b35abd5b7035a9f82dc925941
stone1234765 <51057074+stone1234765@users.noreply.github.com>
2021-03-18 19:32:49 +0200

    add grpc cache

---
+++ SunnyFlamingo/ValueObjects/GrpcGoodsInformationFiles/GoodsInformationFactory.cs
@@ -0,0 +1,159 @@
+﻿using SunnyFlamingo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunnyFlamingo.ValueObjects.GrpcGoodsInformationFiles
+{
+    public class GoodsInformationFactory
+    {
+        public GoodsInformation<string> CreateGoodsInformation(GrpcCacheGoodsInformation cacheGoodsInformation)
+        {
+            var goodsInformation = new GoodsInformation<string>()
+            {
+                Count = cacheGoodsInformation.GoodsInformation.Count,
+            };
+            if (cacheGoodsInformation.GoodsInformation.GoodCells != null)
+            {
+                var goodCells = new List<GoodCellModel>();
+                goodCells.AddRange(cacheGoodsInformation.GoodsInformation.GoodCells.Select(ggc => GetGoodCellModel(ggc)));
+                goodsInformation.GoodCells = goodCells;
+            }
+            if (cacheGoodsInformation.GoodsInformation.Questions != null)
+            {
+                var questions = new List<QuestionsBase<string>>();
+                questions.AddRange(cacheGoodsInformation.GoodsInformation.Questions.Select(gq => GetQuestions(gq)));
+                goodsInformation.Questions = questions;
+            }
+            return goodsInformation;
+        }
+        private QuestionsBase<string> GetQuestions(GrpcQuestionsBase grpcQuestionsBase)
+        {
+            var questions = new QuestionsBase<string>()
+            {
+                Key = grpcQuestionsBase.Key,
+                Order = grpcQuestionsBase.Order,
+                Value = grpcQuestionsBase.Value
+            };
+            if (grpcQuestionsBase.QuestionBaseList != null)
+            {
+                var questionsList = new List<QuestionBase<string>>();
+                questionsList.AddRange(grpcQuestionsBase.QuestionBaseList.Select(q => GetQuestion(q)));
+                questions.QuestionBaseList = questionsList;
+            }
+            return questions;
+        }
+        private QuestionBase<string> GetQuestion(GrpcQuestionBase grpcQuestion)
+        {
+            var question = new QuestionBase<string>()
+            {
+                Key = grpcQuestion.Key,
+                Order = grpcQuestion.Order,
+                Checked = grpcQuestion.Checked,
+                AfterBox = grpcQuestion.AfterBox,
+                Label = grpcQuestion.Label,
+                MethodName = grpcQuestion.MethodName,
+                QuestionsKey = grpcQuestion.QuestionsKey,
+                Value = grpcQuestion.Value,
+                ControlType = (ControlType)grpcQuestion.ControlType,
+                Type = (InputType)grpcQuestion.Type,
+                Validator = GetQuestionValidator(grpcQuestion.Validator)
+            };
+            if (grpcQuestion.Options != null)
+            {
+                var options = new List<QuestionOption>();
+                options.AddRange(grpcQuestion.Options.Select(o => GetQuestionOption(o)));
+                question.Options = options;
+            }
+            switch (grpcQuestion.SelectedFromCase)
+            {
+                case GrpcQuestionBase.SelectedFromOneofCase.SelectedFromInt:
+                    question.SelectedFrom = grpcQuestion.SelectedFromInt;
+                    break;
+                case GrpcQuestionBase.SelectedFromOneofCase.SelectedFromDouble:
+                    question.SelectedFrom = grpcQuestion.SelectedFromDouble;
+                    break;
+                case GrpcQuestionBase.SelectedFromOneofCase.SelectedFromDecimal:
+                    question.SelectedFrom = CustomTypes.DecimalValue.GetDecimal(grpcQuestion.SelectedFromDecimal);
+                    break;
+            }
+            switch (grpcQuestion.SelectedToCase)
+            {
+                case GrpcQuestionBase.SelectedToOneofCase.SelectedToInt:
+                    question.SelectedTo = grpcQuestion.SelectedToInt;
+                    break;
+                case GrpcQuestionBase.SelectedToOneofCase.SelectedToDouble:
+                    question.SelectedTo = grpcQuestion.SelectedToDouble;
+                    break;
+                case GrpcQuestionBase.SelectedToOneofCase.SelectedToDecimal:
+                    question.SelectedTo = CustomTypes.DecimalValue.GetDecimal(grpcQuestion.SelectedToDecimal);
+                    break;
+            }
+            switch (grpcQuestion.FromCase)
+            {
+                case GrpcQuestionBase.FromOneofCase.FromInt:
+                    question.From = grpcQuestion.FromInt;
+                    break;
+                case GrpcQuestionBase.FromOneofCase.FromDouble:
+                    question.From = grpcQuestion.FromDouble;
+                    break;
+                case GrpcQuestionBase.FromOneofCase.FromDecimal:
+                    question.From = CustomTypes.DecimalValue.GetDecimal(grpcQuestion.FromDecimal);
+                    break;
+            }
+            switch (grpcQuestion.ToCase)
+            {
+                case GrpcQuestionBase.ToOneofCase.ToInt:
+                    question.To = grpcQuestion.ToInt;
+                    break;
+                case GrpcQuestionBase.ToOneofCase.ToDouble:
+                    question.To = grpcQuestion.ToDouble;
+                    break;
+                case GrpcQuestionBase.ToOneofCase.ToDecimal:
+                    question.To = CustomTypes.DecimalValue.GetDecimal(grpcQuestion.ToDecimal);
+                    break;
+            }
+            return question;
+        }
+        private QuestionValidator GetQuestionValidator(GrpcQuestionValidator grpcQuestionValidator)
+        {
+            if (grpcQuestionValidator == null)
+            {
+                return null;
+            }
+            return new QuestionValidator()
+            {
+                Max = grpcQuestionValidator.Max,
+                Min = grpcQuestionValidator.Min,
+                MaxLength = grpcQuestionValidator.MaxLength,
+                MinLength = grpcQuestionValidator.MinLength,
+                Required = grpcQuestionValidator.Required
+            };
+        }
+        private QuestionOption GetQuestionOption(GrpcQuestionOption grpcQuestionOption)
+        {
+            return new QuestionOption()
+            {
+                Key = grpcQuestionOption.Key,
+                Value = grpcQuestionOption.Value
+            };
+        }
+        private GoodCellModel GetGoodCellModel(GrpcGoodCellModel grpcGoodCellModel)
+        {
+            var goodCellModel = new GoodCellModel()
+            {
+                Id = Guid.Parse(grpcGoodCellModel.Id),
+                Name = grpcGoodCellModel.Name,
+                IsAvailable = grpcGoodCellModel.IsAvailable,
+                Price = CustomTypes.DecimalValue.GetDecimal(grpcGoodCellModel.Price),
+                Type = grpcGoodCellModel.Type
+            };
+            if (!string.IsNullOrEmpty(grpcGoodCellModel.ImgId))
+            {
+                goodCellModel.ImgId = Guid.Parse(grpcGoodCellModel.ImgId);
+            }
+            return goodCellModel;
+        }
+    }
+}
---
+++ SunnyFlamingo/ValueObjects/GrpcGoodsInformationFiles/GrpcCacheGoodsInformationFactory.cs
@@ -0,0 +1,174 @@
+﻿using Google.Protobuf.Reflection;
+using Google.Protobuf.WellKnownTypes;
+using SunnyFlamingo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SunnyFlamingo.ValueObjects.GrpcGoodsInformationFiles
+{
+    public class GrpcCacheGoodsInformationFactory
+    {
+        public GrpcCacheGoodsInformation CreateGrpcCacheGoodsInformation(string key, GoodsInformation<string> goodsInformation)
+        {
+            var cacheGoodsInformation = new GrpcCacheGoodsInformation()
+            {
+                Key = new Key() { Key_ = key },
+                GoodsInformation = new GrpcGoodsInformation()
+                {
+                    Count = goodsInformation.Count,
+                }
+            };
+            if (goodsInformation.GoodCells != null)
+            {
+                cacheGoodsInformation.GoodsInformation.GoodCells.AddRange(goodsInformation.GoodCells.Select(gc => GetGrpcGoodCellModel(gc)));
+            }
+            if (goodsInformation.Questions != null)
+            {
+                cacheGoodsInformation.GoodsInformation.Questions.AddRange(goodsInformation.Questions.Select(q => GetGrpcQuestions(q)));
+            }
+            return cacheGoodsInformation;
+        }
+        private GrpcQuestionsBase GetGrpcQuestions(QuestionsBase<string> questions)
+        {
+            var grpcQuestionsBase = new GrpcQuestionsBase()
+            {
+                Key = questions.Key,
+                Order = questions.Order,
+                Value = questions.Value
+            };
+            if (questions.QuestionBaseList != null)
+            {
+                grpcQuestionsBase.QuestionBaseList.AddRange(questions.QuestionBaseList.Select(q => GetGrpcQuestion(q)));
+            }
+            return grpcQuestionsBase;
+        }
+        private GrpcQuestionBase GetGrpcQuestion(QuestionBase<string> question)
+        {
+            var grpcQuestionsBase = new GrpcQuestionBase()
+            {
+                Key = question.Key ?? "",
+                Order = question.Order,
+                Checked = question.Checked,
+                AfterBox = question.AfterBox,
+                Label = question.Label ?? "",
+                MethodName = question.MethodName ?? "",
+                QuestionsKey = question.QuestionsKey ?? "",
+                Value = question.Value ?? "",
+                ControlType = (GrpcControlType)question.ControlType,
+                Type = (GrpcInputType)question.Type,
+                Validator = GetGrpcQuestionValidator(question.Validator)
+            };
+            if (question.Options != null)
+            {
+                grpcQuestionsBase.Options.AddRange(question.Options.Select(o => GetGrpcQuestionOption(o)));
+            }
+            if (question.SelectedFrom != null)
+            {
+                if (question.SelectedFrom.GetType() == typeof(int))
+                {
+                    grpcQuestionsBase.SelectedFromInt = (int)question.SelectedFrom;
+                }
+                else if (question.SelectedFrom.GetType() == typeof(double))
+                {
+                    grpcQuestionsBase.SelectedFromDouble = (double)question.SelectedFrom;
+                }
+                else if (question.SelectedFrom.GetType() == typeof(decimal))
+                {
+                    CustomTypes.DecimalValue selectedFrom = (decimal)question.SelectedFrom;
+                    grpcQuestionsBase.SelectedFromDecimal = new DecimalValue() { Nanos = selectedFrom.Nanos, Units = selectedFrom.Units };
+                }
+            }
+            if (question.SelectedTo != null)
+            {
+                if (question.SelectedTo.GetType() == typeof(int))
+                {
+                    grpcQuestionsBase.SelectedToInt = (int)question.SelectedTo;
+                }
+                else if (question.SelectedTo.GetType() == typeof(double))
+                {
+                    grpcQuestionsBase.SelectedToDouble = (double)question.SelectedTo;
+                }
+                else if (question.SelectedTo.GetType() == typeof(decimal))
+                {
+                    CustomTypes.DecimalValue selectedTo = (decimal)question.SelectedTo;
+                    grpcQuestionsBase.SelectedToDecimal = new DecimalValue() { Nanos = selectedTo.Nanos, Units = selectedTo.Units };
+                }
+            }
+            if (question.From != null)
+            {
+                if (question.From.GetType() == typeof(int))
+                {
+                    grpcQuestionsBase.FromInt = (int)question.From;
+                }
+                else if (question.From.GetType() == typeof(double))
+                {
+                    grpcQuestionsBase.FromDouble = (double)question.From;
+                }
+                else if (question.From.GetType() == typeof(decimal))
+                {
+                    CustomTypes.DecimalValue from = (decimal)question.From;
+                    grpcQuestionsBase.FromDecimal = new DecimalValue() { Nanos = from.Nanos, Units = from.Units };
+                }
+            }
+            if (question.To != null)
+            {
+                if (question.To.GetType() == typeof(int))
+                {
+                    grpcQuestionsBase.ToInt = (int)question.To;
+                }
+                else if (question.To.GetType() == typeof(double))
+                {
+                    grpcQuestionsBase.ToDouble = (double)question.To;
+                }
+                else if (question.To.GetType() == typeof(decimal))
+                {
+                    CustomTypes.DecimalValue to = (decimal)question.To;
+                    grpcQuestionsBase.ToDecimal = new DecimalValue() { Nanos = to.Nanos, Units = to.Units };
+                }
+            }
+            return grpcQuestionsBase;
+        }
+        private GrpcQuestionValidator GetGrpcQuestionValidator(QuestionValidator questionValidator)
+        {
+            if (questionValidator == null)
+            {
+                return null;
+            }
+            return new GrpcQuestionValidator()
+            {
+                Max = questionValidator.Max,
+                Min = questionValidator.Min,
+                MaxLength = questionValidator.MaxLength,
+                MinLength = questionValidator.MinLength,
+                Required = questionValidator.Required
+            };
+        }
+        private GrpcQuestionOption GetGrpcQuestionOption(QuestionOption questionOption)
+        {
+            return new GrpcQuestionOption()
+            {
+                Key = questionOption.Key,
+                Value = questionOption.Value
+            };
+        }
+        private GrpcGoodCellModel GetGrpcGoodCellModel(GoodCellModel goodCellModel)
+        {
+            CustomTypes.DecimalValue price = goodCellModel.Price;
+            var grpcGoodCellModel = new GrpcGoodCellModel()
+            {
+                Id = goodCellModel.Id.ToString(),
+                Name = goodCellModel.Name,
+                IsAvailable = goodCellModel.IsAvailable,
+                Price = new DecimalValue() { Nanos = price.Nanos, Units = price.Units },
+                Type = goodCellModel.Type
+            };
+            if (goodCellModel.ImgId != null)
+            {
+                grpcGoodCellModel.ImgId = goodCellModel.ImgId.ToString();
+            }
+            return grpcGoodCellModel;
+        }
+    }
+}
